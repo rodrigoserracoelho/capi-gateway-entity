c04008e7c19d9e41dfcb588ce38df385b5a426a1
s4dad\coelhro <me@rodrigo.at>
2019-09-14 20:05:38 +0200

    Adding Id for mongo

--- src/main/java/at/rodrigo/api/gateway/entity/Api.java
+++ src/main/java/at/rodrigo/api/gateway/entity/Api.java
@@ -1,12 +1,15 @@
 package at.rodrigo.api.gateway.entity;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 import java.util.List;
 
 @Data
 public class Api {
 
+    @Id
+    private String id;
     private String endpoint;
     private String name;
     private boolean secured;
